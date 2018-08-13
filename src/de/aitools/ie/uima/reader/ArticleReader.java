ee31552bfc443b69ecace5bd0c0150b07c4aea02
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2018-08-13 23:39:08 +0200

    Ready for debugging

--- src/de/aitools/ie/uima/reader/ArticleReader.java
+++ src/de/aitools/ie/uima/reader/ArticleReader.java
@@ -1,90 +1,220 @@
 package de.aitools.ie.uima.reader;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Iterator;
+import java.util.zip.ZipFile;
 
-import org.apache.uima.UIMAFramework;
 import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.collection.CollectionReader_ImplBase;
+import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceConfigurationException;
 import org.apache.uima.resource.ResourceInitializationException;
-import org.apache.uima.resource.metadata.FsIndexDescription;
-import org.apache.uima.resource.metadata.TypeSystemDescription;
-import org.apache.uima.util.CasCreationUtils;
-import org.apache.uima.util.InvalidXMLException;
 import org.apache.uima.util.Progress;
 import org.apache.uima.util.ProgressImpl;
-import org.apache.uima.util.XMLInputSource;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import de.aitools.ie.uima.type.core.Date;
+import de.aitools.ie.uima.type.core.Paragraph;
+import de.aitools.ie.uima.type.news.ArticleMetadata;
+import de.aitools.ie.uima.type.news.Link;
+import de.aitools.ie.uima.type.news.Quotation;
+import de.aitools.ie.uima.type.supertype.Unit;
+import de.aitools.util.uima.Pipeline;
 
 public class ArticleReader extends CollectionReader_ImplBase {
   
-  public static final String TYPE_SYSTEM =
-      "uima/typesystems/NewsArticleTypeSystem.xml";
+  // -------------------------------------------------------------------------
+  // CONSTANTS
+  // -------------------------------------------------------------------------
+  
+  public static final String TYPE_SYSTEM_NEWS_ARTICLES =
+      Pipeline.DESCRIPTOR_PACKAGE_FOR_TYPE_SYSTEMS +
+      "NewsArticleTypeSystem.xml";
   
   public static final String TAG_ARTICLE = "article";
   
+  public static final String ATTRIBUTE_ARTICLE_ID = "id";
+  
+  public static final String ATTRIBUTE_ARTICLE_TITLE = "title";
+  
+  public static final String ATTRIBUTE_ARTICLE_PUBLISHED_AT = "published-at";
+  
+  public static final String TAG_LINK = "a";
+  
+  public static final String ATTRIBUTE_LINK_HREF = "href";
+  
+  public static final String ATTRIBUTE_LINK_TYPE = "type";
+  
+  public static final String ATTRIBUTE_LINK_EXTERNAL = "external";
+  
+  public static final String ATTRIBUTE_LINK_INTERNAL = "internal";
+  
+  public static final String TAG_PARAGRAPH = "p";
+  
+  public static final String TAG_QUOTATION = "q";
+  
+  // -------------------------------------------------------------------------
+  // UIMA PARAMETERS
+  // -------------------------------------------------------------------------
+
+  /**
+   * Parameter that specifies the path of the directory or file with the texts
+   * to be processed.
+   */
+  public static final String PARAM_INPUT =
+      Pipeline.DEFAULT_COLLECTION_READER_INPUT_PARAMETER;
+
+  /**
+   * Parameter that specifies the encoding of the texts to be processed.
+   */
+  public static final String PARAM_INPUT_ENCODING = "encoding";
+  
+  // -------------------------------------------------------------------------
+  // MEMBERS
+  // -------------------------------------------------------------------------
+  
   private Iterator<Element> articleIterator;
   
-  private CAS cas;
-  
   private int read;
+  
+  // -------------------------------------------------------------------------
+  // CONSTRUCTORS
+  // -------------------------------------------------------------------------
+  
+  public ArticleReader() {
+    this.articleIterator = null;
+    this.read = 0;
+  }
+  
+  // -------------------------------------------------------------------------
+  // FUNCTIONALITY
+  // -------------------------------------------------------------------------
   
   @Override
   public void reconfigure() throws ResourceConfigurationException {
     super.reconfigure();
-    this.cas = this.createCas();
+    try {
+      final String encoding = (String)
+          this.getConfigParameterValue(PARAM_INPUT_ENCODING);
+      final Charset charset = encoding == null
+          ? Charset.defaultCharset()
+          : Charset.forName(encoding);
+      
+      final ZipFile zipFile = new ZipFile(
+          (String) this.getConfigParameterValue(PARAM_INPUT), charset);
+      this.articleIterator = new ArticleCollectionIterator(zipFile);
+      this.read = 0;
+    } catch (final IOException e) {
+      throw new ResourceConfigurationException(e);
+    }
+  }
+  
+  @Override
+  public void initialize() throws ResourceInitializationException {
+    super.initialize();
     this.read = 0;
-    // TODO this.articleIterator = ;
   }
   
 
   @Override
   public void getNext(final CAS cas) throws IOException, CollectionException {
-    // TODO Auto-generated method stub
-    
+    try {
+      cas.reset();
+      final JCas jcas = cas.getJCas();
+      final StringBuilder text = new StringBuilder();
+
+      final Element articleElement = this.articleIterator.next();
+      this.addMetadata(articleElement, jcas);
+      this.addNode(articleElement, jcas, text);
+      jcas.setDocumentText(text.toString());
+    } catch (final CASException e) {
+      throw new CollectionException(e);
+    }
+  }
+  
+  protected void addMetadata(final Element articleElement, final JCas jcas) {
+    final ArticleMetadata metadata = new ArticleMetadata(jcas);
+    metadata.setId(Integer.parseInt(
+        articleElement.getAttribute(ATTRIBUTE_ARTICLE_ID)));
+
+    final Date publishedAt = new Date(jcas);
+    publishedAt.setNormalized(
+        articleElement.getAttribute(ATTRIBUTE_ARTICLE_PUBLISHED_AT));
+    publishedAt.addToIndexes();
+    metadata.setPublishedAt(publishedAt);
+
+    metadata.setTitle(articleElement.getAttribute(ATTRIBUTE_ARTICLE_TITLE));
+
+    metadata.addToIndexes();
+  }
+  
+  protected void addNode(
+      final Node node, final JCas jcas, final StringBuilder text)
+  throws CASException {
+    final NodeList childNodes = node.getChildNodes();
+    for (int n = 0; n < childNodes.getLength(); ++n) {
+      final Node childNode = childNodes.item(n);
+      switch (childNode.getNodeType()) {
+      case Node.TEXT_NODE:
+      case Node.ENTITY_NODE:
+      case Node.ENTITY_REFERENCE_NODE:
+        text.append(childNode.getTextContent());
+        break;
+      case Node.ELEMENT_NODE:
+        final Element childElement = (Element) childNode;
+        final Unit unit = this.getElementAnnotation(childElement, jcas);
+        unit.setBegin(text.length());
+        this.addNode(childNode, jcas, text);
+        unit.setEnd(text.length());
+        unit.addToIndexes();
+        break;
+      }
+    }
+  }
+  
+  protected Unit getElementAnnotation(final Element element, final JCas jcas) {
+    switch (element.getTagName()) {
+    case TAG_PARAGRAPH:
+      return new Paragraph(jcas);
+    case TAG_QUOTATION:
+      return new Quotation(jcas);
+    case TAG_LINK:
+      final Link link = new Link(jcas);
+      final String type = element.getAttribute(ATTRIBUTE_LINK_TYPE);
+      if (type.isEmpty() || type.equals(ATTRIBUTE_LINK_EXTERNAL)) {
+        link.setExternal(true);
+        link.setHref(element.getAttribute(ATTRIBUTE_LINK_HREF));
+      } else {
+        link.setExternal(false);
+      }
+      return link;
+    }
+    throw new IllegalArgumentException("Unknown tag: " + element.getTagName());
   }
 
   @Override
-  public boolean hasNext() throws IOException, CollectionException {
+  public boolean hasNext() {
+    if (this.articleIterator == null) { return false; }
     return this.articleIterator.hasNext();
   }
 
   @Override
   public Progress[] getProgress() {
     final int totalIsUnknown = -1;
+    final int total = this.hasNext() ? totalIsUnknown : this.read;
     return new Progress[] {
-        new ProgressImpl(this.read, totalIsUnknown, "articles")
+        new ProgressImpl(this.read, total, "articles")
     };
   }
 
   @Override
   public void close() throws IOException {
     this.articleIterator = null;
-  }
-  
-  protected CAS createCas() throws ResourceConfigurationException {
-    try {
-      final URL typeSystemUrl = this.getClass().getClassLoader()
-          .getResource(TYPE_SYSTEM);
-      if (typeSystemUrl == null) {
-        throw new FileNotFoundException("No such type system in class path: "
-            + TYPE_SYSTEM);
-      }
-      
-      final XMLInputSource typeSystemSource = new XMLInputSource(typeSystemUrl);
-      final TypeSystemDescription typeSystem = UIMAFramework.getXMLParser()
-          .parseTypeSystemDescription(typeSystemSource);
-      typeSystem.resolveImports();
-      final CAS cas = CasCreationUtils.createCas(
-          typeSystem, null, new FsIndexDescription[0]);
-      return cas;
-    } catch (final IOException | InvalidXMLException | ResourceInitializationException e) {
-      throw new ResourceConfigurationException(e);
-    }
   }
 
 }
---
+++ src/de/aitools/util/uima/Pipeline.java
@@ -0,0 +1,169 @@
+package de.aitools.util.uima;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Logger;
+
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.ResourceSpecifier;
+import org.apache.uima.resource.metadata.ConfigurationParameterSettings;
+import org.apache.uima.resource.metadata.FsIndexDescription;
+import org.apache.uima.resource.metadata.ProcessingResourceMetaData;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.apache.uima.util.CasCreationUtils;
+import org.apache.uima.util.InvalidXMLException;
+import org.apache.uima.util.XMLInputSource;
+
+/**
+ * Utility class for creating UIMA objects used in UIMA pipelines.
+ *
+ * @author johannes.kiesel@uni-weimar.de
+ */
+public class Pipeline {
+  
+  // -------------------------------------------------------------------------
+  // LOGGING
+  // -------------------------------------------------------------------------
+  
+  private static final Logger LOG =
+      Logger.getLogger(Pipeline.class.getName());
+  
+  // -------------------------------------------------------------------------
+  // CONSTANTS
+  // -------------------------------------------------------------------------
+
+  /**
+   * Default name for the parameter used to specify the input directory or file
+   * of a collection reader.
+   */
+  public static final String DEFAULT_COLLECTION_READER_INPUT_PARAMETER = 
+      "input";
+  
+  /**
+   * Directory in the class path that contains the primitive analysis engine
+   * descriptors.
+   */
+  public static final String DESCRIPTOR_PACKAGE_FOR_PRIMITIVE_ANALYSIS_ENGINES =
+      "uima.primitives.";
+  
+  /**
+   * Directory in the class path that contains the aggregate analysis engine
+   * descriptors.
+   */
+  public static final String DESCRIPTOR_PACKAGE_FOR_AGGREGATE_ANALYSIS_ENGINES =
+      "uima.aggregates.";
+  
+  /**
+   * Directory in the class path that contains the collection reader
+   * descriptors.
+   */
+  public static final String DESCRIPTOR_PACKAGE_FOR_COLLECTION_READERS =
+      "uima.collectionreaders.";
+  
+  /**
+   * Directory in the class path that contains the type system descriptors.
+   */
+  public static final String DESCRIPTOR_PACKAGE_FOR_TYPE_SYSTEMS =
+      "uima.typesystems.";
+  
+  // -------------------------------------------------------------------------
+  // CONSTRUCTOR
+  // -------------------------------------------------------------------------
+  
+  protected Pipeline() {}
+  
+  // -------------------------------------------------------------------------
+  // FUNCTIONALITY
+  // -------------------------------------------------------------------------
+
+  /**
+   * Creates a new CAS object for given type system.
+   * @param typeSystemPath The path of the XML file of the CAS' type system
+   * @return The CAS object
+   * @throws InvalidXMLException If the type system file is not valid
+   * @throws IOException If the type system file could not be found or read
+   * @throws ResourceInitializationException If the CAS could not be initialized
+   */
+  public static CAS createCas(final String typeSystemPath)
+  throws InvalidXMLException, IOException, ResourceInitializationException {
+    LOG.info("Initializing CAS for " + typeSystemPath);
+
+    final URL typeSystemUrl = ClassLoader.getSystemResource(typeSystemPath);
+    if (typeSystemUrl == null) {
+      throw new FileNotFoundException("No such type system in class path: "
+          + typeSystemPath);
+    }
+    
+    final XMLInputSource typeSystemSource = new XMLInputSource(typeSystemUrl);
+    final TypeSystemDescription typeSystem = UIMAFramework.getXMLParser()
+        .parseTypeSystemDescription(typeSystemSource);
+    typeSystem.resolveImports();
+    return CasCreationUtils.createCas(
+        typeSystem, null, new FsIndexDescription[0]);
+  }
+
+  /**
+   * Creates a new collection reader.
+   * @param collectionReaderPath The path of the XML file of the collection
+   * reader to be used to iterate over all files to be processed
+   * @param inputParameter The name of the parameter of the collection reader
+   * that specifies the input file or directory
+   * @param input The input file or directory to read from
+   * @return The created collection reader
+   * @throws IOException If the collection reader file could not be read
+   * @throws InvalidXMLException If the collection reader file is not valid
+   * @throws ResourceInitializationException If the collection reader could not
+   * be initialized
+   */
+  public static CollectionReader createCollectionReader(
+      final String collectionReaderPath,
+      final String inputParameter, final String input)
+  throws IOException, InvalidXMLException, ResourceInitializationException {
+    LOG.info("Initializing CollectionReader " + collectionReaderPath + " with "
+        + inputParameter + " = " + input);
+
+    final XMLInputSource xmlInputSource =
+        new XMLInputSource(collectionReaderPath);
+    final ResourceSpecifier specifier =
+        UIMAFramework.getXMLParser().parseResourceSpecifier(xmlInputSource);
+
+    final ProcessingResourceMetaData metaData = (ProcessingResourceMetaData)
+        specifier.getAttributeValue("metaData");
+    final ConfigurationParameterSettings settings =
+        metaData.getConfigurationParameterSettings();
+    settings.setParameterValue(inputParameter, input);
+
+    final CollectionReader collectionReader =
+        UIMAFramework.produceCollectionReader(specifier);
+    return collectionReader;
+  }
+
+  /**
+   * Creates a new analysis engine.
+   * @param collectionReaderPath The path of the XML file of the analysis engine
+   * @return The created analysis engine
+   * @throws IOException If the analysis engine file could not be read
+   * @throws InvalidXMLException If the analysis engine file is not valid
+   * @throws ResourceInitializationException If the analysis engine could not be
+   * initialized
+   */
+  public static AnalysisEngine createAnalysisEngine(
+      final String analysisEnginePath)
+  throws IOException, InvalidXMLException, ResourceInitializationException {
+    LOG.info("Initializing AnalysisEngine " + analysisEnginePath);
+
+    final XMLInputSource xmlInputSource =
+        new XMLInputSource(analysisEnginePath);
+    final ResourceSpecifier specifier =
+        UIMAFramework.getXMLParser().parseResourceSpecifier(xmlInputSource);
+    final AnalysisEngine analysisEngine =
+        UIMAFramework.produceAnalysisEngine(specifier);
+    return analysisEngine;
+  }
+
+}
